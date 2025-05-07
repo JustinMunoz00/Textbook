5589027c3f1b62c902d04f5272495ea77063ba7f
JustinMunoz00 <justinm880@nycstudents.net>
2025-05-07 03:19:23 +0000

    finished

---
+++ Book.java
@@ -0,0 +1,26 @@
+public class Book {
+
+
+private String title;
+private double price;
+
+
+public Book (String bookTitle, double bookPrice) {
+    title = bookTitle;
+    price = bookPrice;
+ }
+
+
+public String getTitle() {
+    return title;
+    }
+
+
+public String getBookInfo() {
+return title + "-" + price;
+}
+
+
+
+
+}
